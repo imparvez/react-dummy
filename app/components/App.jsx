9cf18da4bf61090bce7cc24253c1947034615579
Parvez Shaikh <parvezshaikhj@gmail.com>
2017-04-10 23:53:34 +0530

    setState() method is used for updating the state of the component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 
 class App extends React.Component {
+	constructor(){
+		super();
+
+		this.state = {
+			data: [],
+		}
+
+		this.setStateHandler = this.setStateHandler.bind(this);
+	}
+
+	setStateHandler(){
+		var item = "State....";
+		var myArr = this.state.data;
+		myArr.push(item);
+		this.setState({
+			data: myArr
+		})
+	}
+
    render() {
       return (
          <div>
-            <h3>Array: {this.props.propArray}</h3>
+            <button onClick={this.setStateHandler}>SET STATE</button>
+            <h4>{this.state.data}</h4>
          </div>
       );
    }
-}
-
-App.propTypes = {
-   propArray: React.PropTypes.array.isRequired
-}
-
-App.defaultProps = {
-   propArray: [1,2,3,4,5],
 }
 
 export default App;
